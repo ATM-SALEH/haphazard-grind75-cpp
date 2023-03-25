7a9b7dc9806a176e8ce717ae7cdb046a6e71da99
ATM SALEH <saleh.cse08@gmail.com>
2023-03-25 12:28:55 +0000

    Use wrapper cmake target for each problem

--- src/g1/s1.cmake
+++ src/g1/s1.cmake
@@ -1,5 +1,5 @@
-add_library(g1_s1
+add_library(s1
     s1.cpp    
 )
 
-target_include_directories(g1_s1 PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+target_include_directories(s1 PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
