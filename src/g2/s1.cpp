adc8adceb4766d55ea507b8008e006f841d3b62b
ATM SALEH <saleh.cse08@gmail.com>
2023-04-08 12:01:38 +0000

    Add implementation and test case for g2

--- src/g2/s1.cpp
+++ src/g2/s1.cpp
@@ -1,11 +1,40 @@
 #include <s1.h>
+#include <stack>
 
 namespace g2
 {
 
 bool S1::isValid(const std::string& s) const
 {
-    return false;
+    std::stack<char> buffer;
+    for (const char c : s)
+    {
+        if (c == '(')
+        {
+            buffer.push(')');
+        }
+        else if (c == '{')
+        {
+            buffer.push('}');
+        }
+        else if (c == '[')
+        {
+            buffer.push(']');
+        }
+        else
+        {
+            if (buffer.empty())
+            {
+                return false;
+            }
+            if (char top = buffer.top(); top != c)
+            {
+                return false;
+            }
+            buffer.pop();
+        }
+    }
+    return buffer.empty();
 }
 
 } // end of namespace g2
--- src/g2/s1.t.cpp
+++ src/g2/s1.t.cpp
@@ -1,9 +1,40 @@
 #include <s1.h>
 #include <gtest/gtest.h>
 
-TEST(G2S1Tests, ExampleTest)
+using namespace g2;
+
+TEST(G2S1Tests, WithMatchingCurlyBracesAndBrackets)
 {
-    using namespace g2;
     S1 s1;
-    ASSERT_EQ(s1.isValid("[{}]"), false);
+    ASSERT_EQ(s1.isValid("[{}]"), true);
+}
+
+TEST(G2S1Tests, WithNonMatchingCurlyBraceAndBrackets)
+{
+    S1 s1;
+    ASSERT_EQ(s1.isValid("[{]"), false);
+}
+
+TEST(G2S1Tests, WithOpeningParenthesesOnly)
+{
+    S1 s1;
+    ASSERT_EQ(s1.isValid("[{(["), false);
+}
+
+TEST(G2S1Tests, WithInvalidInterleavingParenthesesPairs)
+{
+    S1 s1;
+    ASSERT_EQ(s1.isValid("[{]}"), false);
+}
+
+TEST(G2S1Tests, WithEmptyInput)
+{
+    S1 s1;
+    ASSERT_EQ(s1.isValid(""), true);
+}
+
+TEST(G2S1Tests, WithNonoverlappingOpenClosePairs)
+{
+    S1 s1;
+    ASSERT_EQ(s1.isValid("[]{}"), true);
 }
